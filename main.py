2a62e6ffc9120c335830a25e224ef098383a73e6
Vinz Angelo Madrigal <mvinzangelo@gmail.com>
2022-10-10 16:23:54 -0700

    Init commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# KEY POINTS: 
+# - make sure to keep code general (can solve multiple n's for 8 puzzle)
+# - make can put any inital state
+# 
+
+
+def general_search(problem, queueing_function):
+    # nodes = make_queue(make_node(problem.initial_state))
+    # loop do
+    #       if nodes is empty then return failure
+    #   node = remove_front(nodes) 
+    #       if problem.goal_test(node.state)
+    #   nodes = queuing_function(nodes, EXPAND((problem.OPERATORS))
+    # end
