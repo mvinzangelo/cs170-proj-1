924b8e927503884420e2c3eea3e469b8aa7bf70a
Vinz Angelo Madrigal <mvinzangelo@gmail.com>
2022-10-10 17:08:24 -0700

    Add Eight_Puzzle_Problem class

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 # KEY POINTS: 
 # - make sure to keep code general (can solve multiple n's for 8 puzzle)
 # - make can put any inital state
-# 
 
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.children = [None]
 
+class Eight_Puzzle_Problem: 
+    initial_state = [[1,2,3],[4,5,6],[7,8,0]]
+    
+    
+
+    
 def general_search(problem, queueing_function):
     # nodes = make_queue(make_node(problem.initial_state))
     # loop do
@@ -12,3 +21,6 @@ def general_search(problem, queueing_function):
     #       if problem.goal_test(node.state)
     #   nodes = queuing_function(nodes, EXPAND((problem.OPERATORS))
     # end
+    
+
+general_search(Eight_Puzzle_Problem, None)
