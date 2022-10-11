c680baa1bf2c7e463c5992360010279a86187b0c
Vinz Angelo Madrigal <mvinzangelo@gmail.com>
2022-10-11 10:27:11 -0700

    Add board class

--- main.py
+++ main.py
@@ -2,20 +2,25 @@
 # - make sure to keep code general (can solve multiple n's for 8 puzzle)
 # - make can put any inital state
 
+class Board:
+    def __init__(self, state, zero):
+        self.state = state
+        self.zero = zero
+
 class Node:
     def __init__(self, val):
         self.val = val
         self.children = [None]
 
 class Eight_Puzzle_Problem: 
-    initial_state = [[1,2,3],[4,5,6],[7,8,0]]
-    goal_state = [[1,2,3],[4,5,6],[7,8,0]]
+    initial_state = Board([[1,2,3],[4,5,6],[7,8,0]], {"x": 2, "y": 2})
+    goal_state = Board([[1,2,3],[4,5,6],[7,8,0]], {"x": 2, "y": 2})
     def goal_test(self, sample):
-        return sample == self.goal_state
+        return sample.state == self.goal_state.state
 
-def hit
+# def expand(node, operators)
 
-def uniform_cost_search(nodes, expand_function)
+# def uniform_cost_search(nodes, expand_function)
 
     
 def general_search(problem, queueing_function):
@@ -30,9 +35,8 @@ def general_search(problem, queueing_function):
         curr_node = nodes.pop(0)
     #   if problem.goal_test(node.state)
         if problem.goal_test(curr_node.val):
-            return curr_node.val
+            return curr_node.val.state
     #   nodes = queuing_function(nodes, EXPAND(node, problem.OPERATORS))
-        nodes = 
     # end
 
 
